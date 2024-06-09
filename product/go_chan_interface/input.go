150248bce82ccf4ebfadaa249a6244dfd8041e3e
Alexander Yakushev <yakushev6400@gmail.com>
2024-06-09 10:24:11 +0300

    go chan interface task

---
+++ product/go_chan_interface/input.go
@@ -0,0 +1,16 @@
+package gochaninterface
+
+var ListOfLines = []string{
+	"Hello Everyone!",
+	"Let's make THIS task",
+	"a bit cooler than others",
+	"YoU WiLL NeeD tO Make",
+	"Simple BUT interesting program",
+	"Do Your BEST",
+	"GoOd LuCk!",
+}
+
+var (
+	UppercaseLines = []string{}
+	ReverseLines   = []string{}
+)
